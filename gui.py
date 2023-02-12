84d2a03e0b1791d1883c031e263fb69cc29d2294
Borek2002 <damianogdf@gmail.com>
2023-02-13 00:05:17 +0100

    Adding 3 pagesStart Login Add User

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from PIL import ImageTk,Image
 
+BUTTONHEIGHT=2
+BUTTONWIDTH=30
+FONT="Comic Sans MS"
+BACKGROUNDIMG='photo.png'
+
 
 class MainGUI(tk.Tk):
     def __init__(self):
@@ -8,9 +13,9 @@ class MainGUI(tk.Tk):
         self._frame = None
         self.switch_frame(StartPage)
         self.geometry('750x500')
-
+        self.title("FaceID Luncher")
+        
     def switch_frame(self, frame_class):
-        """Destroys current frame and replaces it with a new one."""
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
@@ -22,33 +27,72 @@ class StartPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
 
-        self.bg_image = ImageTk.PhotoImage(Image.open('photo.png'))
+        #background image
+        self.bg_image = ImageTk.PhotoImage(Image.open(BACKGROUNDIMG))
         self.bg_label = tk.Label(self, image=self.bg_image)
         self.bg_label.place(x=0, y=0)
+        canvas=tk.Canvas(self,width=750,height=45,bg="black")
+        canvas.place(x=0,y=0)
 
-        button1 = tk.Button(self, text="Go to Page One", command=lambda: master.switch_frame(PageOne))
-        button1.pack()
+        #title
+        text_canvas=canvas.create_text(320,5,anchor="nw",fill="white")
+        canvas.itemconfig(text_canvas,text="Luncher",font = ((FONT),25))
+
+        #buttons
+        button1 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Login", command=lambda: master.switch_frame(LoginPage)
+                            ,bg="white",fg="black", relief="solid",borderwidth=2,font = ((FONT),10))
+        button2 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Add User",command=lambda: master.switch_frame(RegPage)
+                            ,bg="white",fg="black",relief="solid", borderwidth=2,font = ((FONT),10))
+        button1.place(x=100,y=300)
+        button2.place(x=400,y=300)
 
 
-class PageOne(tk.Frame):
+class LoginPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        self.bg_image = ImageTk.PhotoImage(Image.open('photo.png'))
+
+        #background image
+        self.bg_image = ImageTk.PhotoImage(Image.open(BACKGROUNDIMG))
         self.bg_label = tk.Label(self, image=self.bg_image)
         self.bg_label.place(x=0, y=0)
+        canvas=tk.Canvas(self,width=750,height=45,bg="black")
+        canvas.place(x=0,y=0)
 
-        tk.Button(self, text="Page 2",  command=lambda: master.switch_frame(PageTwo)).pack()
+        #title
+        text_canvas=canvas.create_text(320,5,anchor="nw",fill="white")
+        canvas.itemconfig(text_canvas,text="Login",font = ((FONT),25))
+
+        #buttons
+        button1 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Start Menu", command=lambda: master.switch_frame(StartPage)
+                            ,bg="white",fg="black", relief="solid",borderwidth=2,font = ((FONT),10))
+        button2 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Add User",command=lambda: master.switch_frame(RegPage)
+                            ,bg="white",fg="black",relief="solid", borderwidth=2,font = ((FONT),10))
+        button1.place(x=100,y=400)
+        button2.place(x=400,y=400)
 
 
-class PageTwo(tk.Frame):
+class RegPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
 
-        self.bg_image =ImageTk.PhotoImage(Image.open('photo.png'))
+        #background image
+        self.bg_image = ImageTk.PhotoImage(Image.open(BACKGROUNDIMG))
         self.bg_label = tk.Label(self, image=self.bg_image)
         self.bg_label.place(x=0, y=0)
+        canvas=tk.Canvas(self,width=750,height=45,bg="black")
+        canvas.place(x=0,y=0)
 
-        tk.Button(self, text="Return to start page", command=lambda: master.switch_frame(StartPage)).pack()
+        #title
+        text_canvas=canvas.create_text(320,5,anchor="nw",fill="white")
+        canvas.itemconfig(text_canvas,text="Add User",font = ((FONT),25))
+
+        #buttons
+        button1 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Login", command=lambda: master.switch_frame(LoginPage)
+                            ,bg="white",fg="black", relief="solid",borderwidth=2,font = ((FONT),10))
+        button2 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Start Menu",command=lambda: master.switch_frame(StartPage)
+                            ,bg="white",fg="black",relief="solid", borderwidth=2,font = ((FONT),10))
+        button1.place(x=100,y=400)
+        button2.place(x=400,y=400)
 
 
 
