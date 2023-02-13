f925122c61d26b3b2e3089604d820c9ca62f9b20
Borek2002 <damianogdf@gmail.com>
2023-02-13 15:30:58 +0100

    Database and login software

---
+++ database.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+
+def create_table():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    query = "DROP TABLE IF EXISTS login"
+    cursor.execute(query)
+    conn.commit()
+
+    query = "CREATE TABLE login(username VARCHAR UNIQUE, password VARCHAR)"
+    cursor.execute(query)
+    conn.commit()
+
+def add_user(username, password):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    query = "INSERT INTO login (username, password) VALUES (?, ?)"
+    cursor.execute(query, (username, password))
+    conn.commit()
+
+def check_user(username, password):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    query = 'SELECT * FROM login WHERE username = ? AND password = ?'
+    cursor.execute(query, (str(username), str(password)))
+    result = cursor.fetchone()
+    conn.commit()
+    print('[DEBUG][check] result:', result)
+    return result
+
+def login(username, password):
+        if check_user(username, password):
+            print("Username correct!")
+            print("Password correct!")
+            print("Logging in...")
+        else:
+            print("Something wrong")
+
+# --- main ---
+
+
+
+
+#create_table()  # use only once
+
+
+#add_user("admin", "admin")
+
+
--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from PIL import ImageTk,Image
-
+import database as db
 BUTTONHEIGHT=2
 BUTTONWIDTH=30
 FONT="Comic Sans MS"
@@ -14,13 +14,14 @@ class MainGUI(tk.Tk):
         self.switch_frame(StartPage)
         self.geometry('750x500')
         self.title("FaceID Luncher")
-        
+
     def switch_frame(self, frame_class):
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
         self._frame = new_frame
         self._frame.pack(fill="both",expand=1)
+
 
 
 class StartPage(tk.Frame):
@@ -39,18 +40,19 @@ class StartPage(tk.Frame):
         canvas.itemconfig(text_canvas,text="Luncher",font = ((FONT),25))
 
         #buttons
-        button1 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Login", command=lambda: master.switch_frame(LoginPage)
+        loginButton = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Login Page", command=lambda: master.switch_frame(LoginPage)
                             ,bg="white",fg="black", relief="solid",borderwidth=2,font = ((FONT),10))
-        button2 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Add User",command=lambda: master.switch_frame(RegPage)
+        addUserButton = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Add User",command=lambda: master.switch_frame(RegPage)
                             ,bg="white",fg="black",relief="solid", borderwidth=2,font = ((FONT),10))
-        button1.place(x=100,y=300)
-        button2.place(x=400,y=300)
+        loginButton.place(x=100,y=300)
+        addUserButton.place(x=400,y=300)
 
 
 class LoginPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-
+        userLoginInput=tk.StringVar()
+        userPassInput=tk.StringVar()
         #background image
         self.bg_image = ImageTk.PhotoImage(Image.open(BACKGROUNDIMG))
         self.bg_label = tk.Label(self, image=self.bg_image)
@@ -60,15 +62,31 @@ class LoginPage(tk.Frame):
 
         #title
         text_canvas=canvas.create_text(320,5,anchor="nw",fill="white")
-        canvas.itemconfig(text_canvas,text="Login",font = ((FONT),25))
+        canvas.itemconfig(text_canvas,text="Login Page",font = ((FONT),25))
 
         #buttons
-        button1 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Start Menu", command=lambda: master.switch_frame(StartPage)
+        startButton = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Start Menu", command=lambda: master.switch_frame(StartPage)
                             ,bg="white",fg="black", relief="solid",borderwidth=2,font = ((FONT),10))
-        button2 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Add User",command=lambda: master.switch_frame(RegPage)
+        addUserButton = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Add User",command=lambda: master.switch_frame(RegPage)
                             ,bg="white",fg="black",relief="solid", borderwidth=2,font = ((FONT),10))
-        button1.place(x=100,y=400)
-        button2.place(x=400,y=400)
+
+        startButton.place(x=100,y=400)
+        addUserButton.place(x=400,y=400)
+
+        #login form
+        loginText=tk.Label(self,text="Username",font = ((FONT),15))
+        loginText.place(x=80,y=100,height=30,width=100)
+        loginInput=tk.Entry(self,textvariable=userLoginInput)
+        loginInput.place(x=200,y=100,height=30)
+
+        passText=tk.Label(self,text="Password",font = ((FONT),15))
+        passText.place(x=80,y=140,height=30,width=100)
+        passInput=tk.Entry(self,textvariable=userPassInput)
+        passInput.place(x=200,y=140,height=30)
+
+        nextButton=tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Next -> FaceID",command=lambda: db.login(userLoginInput.get(),userPassInput.get())
+                            ,bg="white",fg="black",relief="solid", borderwidth=2,font = ((FONT),10))
+        nextButton.place(x=150, y=180)
 
 
 class RegPage(tk.Frame):
@@ -84,15 +102,15 @@ class RegPage(tk.Frame):
 
         #title
         text_canvas=canvas.create_text(320,5,anchor="nw",fill="white")
-        canvas.itemconfig(text_canvas,text="Add User",font = ((FONT),25))
+        canvas.itemconfig(text_canvas,text="Add User Page",font = ((FONT),25))
 
         #buttons
-        button1 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Login", command=lambda: master.switch_frame(LoginPage)
+        loginButton = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Login", command=lambda: master.switch_frame(LoginPage)
                             ,bg="white",fg="black", relief="solid",borderwidth=2,font = ((FONT),10))
-        button2 = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Start Menu",command=lambda: master.switch_frame(StartPage)
+        startButton = tk.Button(self,height=BUTTONHEIGHT,width=BUTTONWIDTH, text="Start Menu",command=lambda: master.switch_frame(StartPage)
                             ,bg="white",fg="black",relief="solid", borderwidth=2,font = ((FONT),10))
-        button1.place(x=100,y=400)
-        button2.place(x=400,y=400)
+        loginButton.place(x=100,y=400)
+        startButton.place(x=400,y=400)
 
 
 
